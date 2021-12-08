58e63f1211b306e67a92a1f772b702b00ca732e0
patchcervan <francerper@gmail.com>
2021-12-08 15:12:04 +0200

    Discontinue message

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onLoad <- function(libname, pkgname) {
+
+  packageStartupMessage("IMPORTANT: SABAP has been discontinued and will soon disappear. Please, move on to ABAP, https://github.com/AfricaBirdData/ABAP (it is better!).")
+
+  }
